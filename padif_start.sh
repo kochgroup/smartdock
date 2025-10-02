fc90e0d67198050541e74d70a4faa52ff964d12c
Felipe Victoria-Munoz <danielfvictoriam@gmail.com>
2025-10-02 17:00:01 -0500

    instructions update

--- padif_start.sh
+++ padif_start.sh
@@ -3,5 +3,5 @@ export COMPOSE_BAKE=true
 
 docker compose up -d
 
-docker exec -it padif_app /bin/bash -c "/mnt/ccdc/CSDS2020/CSD_2020/bin/ccdc_activator -a -k 96DAD1-E0657B-44F7B2-E17D11-EB9203-EEBCCE"
+docker exec -it padif_app /bin/bash -c "/mnt/ccdc/CSDS2020/CSD_2020/bin/ccdc_activator -a -k YOUR KEY"
 docker exec -it padif_app /bin/bash
